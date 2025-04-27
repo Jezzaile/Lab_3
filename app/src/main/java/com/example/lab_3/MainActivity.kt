345ef35bd0453ea562c1ba259fdc87e7ded9aedc
Jezaile <whocaresmymail@gmail.com>
2025-04-27 15:46:26 +0700

    Добавлена функция invertString, инвертирующая строку

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -9,6 +9,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+//Вариант 8. Дана строка. Инвертировать ее, то есть произвести обмен значениями
+//между первым и последним символами строки,
+//вторым и предпоследним и так далее.
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +33,18 @@ class MainActivity : AppCompatActivity() {
             return
         }
         val st = inp.text.toString()
-        Log.d("test", st)
+        invertString(st)
+    }
+
+    fun invertString (s:String){
+        val len = s.length
+        var res_A:CharSequence = s.subSequence(0,len/2)
+        var res_B:CharSequence = s.subSequence(len/2, len)
+        res_A = res_A.reversed()
+        res_B = res_B.reversed()
+        val out = res_B.toString() + res_A.toString()
+        Log.d("test", out)
+
     }
 }
 
