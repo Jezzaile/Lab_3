eb9e9e1d8dd2644ea051c801cdd978475a3102cd
Jezaile <whocaresmymail@gmail.com>
2025-04-27 15:26:12 +0700

    Добавлена функция onClickButton, принимающая введенную строку и проверяющую, не пуста ли она.

--- app/src/main/java/com/example/lab_3/MainActivity.kt
+++ app/src/main/java/com/example/lab_3/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.lab_3
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,4 +20,14 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    fun onClickButton(@Suppress("UNUSED_PARAMETER")view: View){
+        val inp = findViewById<TextView>(R.id.textInputEditText)
+        if(inp.text.isNullOrBlank()){
+            return
+        }
+        val st = inp.text.toString()
+        Log.d("test", st)
+    }
 }
+
